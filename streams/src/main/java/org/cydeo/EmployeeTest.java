d8351e8dd804ecde22d730dac5574b0f7a57b4c0
makhmudakhmedov34 <akhmedov_makhmud@yahoo.com>
2023-08-21 22:49:40 -0400

    FlatMap

---
+++ streams/src/main/java/org/cydeo/EmployeeTest.java
@@ -0,0 +1,34 @@
+package org.cydeo;
+
+import java.util.List;
+
+public class EmployeeTest {
+    public static void main(String[] args) {
+
+        //Print all emails - One employee has one email - One to One
+        System.out.println("********************************");
+        EmployeeData.readAll()
+                .map(Employee::getEmpName)
+                .forEach(System.out::println);
+
+        // Print all phone numbers
+        System.out.println("********************************");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .forEach(System.out::println);
+
+        //
+        System.out.println("********************************");
+        EmployeeData.readAll()
+                .flatMap(employee -> employee.getEmpPhoneNumbers().stream())
+                .forEach(System.out::println);
+        System.out.println("********************************");
+        EmployeeData.readAll()
+                .map(Employee::getEmpPhoneNumbers)
+                .flatMap(List::stream)
+                .forEach(System.out::println);
+
+
+
+    }
+}
